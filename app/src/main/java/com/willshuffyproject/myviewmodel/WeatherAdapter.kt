495f4e65081a889ac52598d94375a470574718d1
willshuffy <saputrawilly26@gmail.com>
2020-11-11 08:24:17 +0700

    set data on adapter class

--- app/src/main/java/com/willshuffyproject/myviewmodel/WeatherAdapter.kt
+++ app/src/main/java/com/willshuffyproject/myviewmodel/WeatherAdapter.kt
@@ -1,24 +1,41 @@
 package com.willshuffyproject.myviewmodel
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.weather_items.view.*
 
 class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
 
+    private val mData = ArrayList<WeatherItems>()
+
+    fun setData(items: ArrayList<WeatherItems>){
+        mData.clear()
+        mData.addAll(items)
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): WeatherViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val mView = LayoutInflater.from(viewGroup.context).inflate(R.layout.weather_items, viewGroup, false)
+        return WeatherViewHolder(mView)
     }
 
     override fun onBindViewHolder(weatherViewHolder: WeatherViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        weatherViewHolder.bind(mData[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = mData.size
 
     inner class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bind(weatherItems: WeatherItems){
+            with(itemView){
+                textCity.text = weatherItems.name
+                textTemp.text = weatherItems.temperature
+                textDesc.text = weatherItems.description
+            }
+        }
 
     }
 
