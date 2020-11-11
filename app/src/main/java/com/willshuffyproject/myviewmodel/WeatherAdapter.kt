321886d26fc2af87180dbbfb314250dd73d9b6de
willshuffy <saputrawilly26@gmail.com>
2020-11-11 08:11:07 +0700

    adding adapter class

---
+++ app/src/main/java/com/willshuffyproject/myviewmodel/WeatherAdapter.kt
@@ -0,0 +1,25 @@
+package com.willshuffyproject.myviewmodel
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): WeatherViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(weatherViewHolder: WeatherViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
