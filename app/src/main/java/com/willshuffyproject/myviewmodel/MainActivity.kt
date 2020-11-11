8403f9870e8a28ce5fa41fd84457345f0670e1af
willshuffy <saputrawilly26@gmail.com>
2020-11-11 08:38:34 +0700

    initialization adapter on activity

--- app/src/main/java/com/willshuffyproject/myviewmodel/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myviewmodel/MainActivity.kt
@@ -2,11 +2,21 @@ package com.willshuffyproject.myviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 }
