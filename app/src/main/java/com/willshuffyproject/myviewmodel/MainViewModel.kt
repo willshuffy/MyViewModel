0a2c53c7d80e10859c75a9bb0f1e88a7c5c0826d
willshuffy <saputrawilly26@gmail.com>
2020-11-11 10:36:43 +0700

    adding viewmodel to manage orientation change

---
+++ app/src/main/java/com/willshuffyproject/myviewmodel/MainViewModel.kt
@@ -0,0 +1,66 @@
+package com.willshuffyproject.myviewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.text.DecimalFormat
+
+class MainViewModel : ViewModel() {
+
+    val listWeathers = MutableLiveData<ArrayList<WeatherItems>>()
+
+    fun setWeather(cities: String){
+
+        //request API
+
+        val listItems = ArrayList<WeatherItems>()
+
+        val apiKey = "c5bf0e763f34d0ecf9fd0fda46433038"
+        val url = "https://api.openweathermap.org/data/2.5/group?id=$cities&units=metric&appid=${apiKey}"
+
+        val client = AsyncHttpClient()
+        client.get(url, object : AsyncHttpResponseHandler(){
+            override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
+                try {
+                    //parsing json
+                    val result = String(responseBody)
+                    val responseObject = JSONObject(result)
+                    val list = responseObject.getJSONArray("list")
+
+                    for (i in 0 until list.length()){
+
+                        val weather = list.getJSONObject(i)
+                        val weatherItems = WeatherItems()
+                        weatherItems.id = weather.getInt("id")
+                        weatherItems.name = weather.getString("name")
+                        weatherItems.currentWeather = weather.getJSONArray("weather").getJSONObject(0).getString("main")
+                        weatherItems.description = weather.getJSONArray("weather").getJSONObject(0).getString("description")
+                        val tempInKelvin = weather.getJSONObject("main").getDouble("temp")
+                        val tempInCelcius = tempInKelvin -273
+                        weatherItems.temperature = DecimalFormat("##.##").format(tempInCelcius)
+                        listItems.add(weatherItems)
+                    }
+
+                    //post Value ke MutableLiveData
+                   listWeathers.postValue(listItems)
+                }catch (e: Exception){
+                    Log.d("Exception", e.message.toString())
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
+                Log.d("onFailure", error.message.toString())
+            }
+
+        })
+    }
+
+    fun getWeather(): LiveData<ArrayList<WeatherItems>>{
+        return listWeathers
+    }
+}
